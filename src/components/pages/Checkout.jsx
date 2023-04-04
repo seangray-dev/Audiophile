010099ee788709a06033620e83eb78c110156352
Sean Gray <seangray.dev@gmail.com>
2023-04-04 05:35:32 -0400

    add responsive design to chekcout page

--- src/components/pages/Checkout.jsx
+++ src/components/pages/Checkout.jsx
@@ -95,13 +95,13 @@ const Checkout = () => {
         </a>
       </div>
 
-      <form>
-        <div className='container mx-auto max-w-[327px] bg-white px-6 pt-6 pb-8 rounded-lg mb-8'>
+      <form className='mx-2 flex flex-col lg:flex-row lg:gap-[30px] lg:mx-auto lg:max-w-[1110px] lg:px-2'>
+        <div className='container mx-auto max-w-[327px] md:max-w-[689px] lg:max-w-[730px] bg-white px-6 pt-6 pb-8 rounded-lg mb-8 lg:mb-[141px]'>
           <h2 className='text-h4 uppercase mb-8'>Checkout</h2>
           <h3 className='text-brightOrange text-subtitle uppercase mb-4'>
             Billing Details
           </h3>
-          <div className='flex flex-col gap-6 mb-8'>
+          <div className='flex flex-col gap-6 mb-8 md:grid md:grid-cols-2'>
             <div className='flex flex-col gap-2'>
               <label className='text-[12px] font-bold'>Name</label>
               <input
@@ -137,7 +137,7 @@ const Checkout = () => {
           </h3>
           <div className='flex flex-col gap-6 mb-8'>
             <div className='flex flex-col gap-2'>
-              <label className='text-[12px] font-bold'> Your Address</label>
+              <label className='text-[12px] font-bold'>Your Address</label>
               <input
                 className='border border-silver rounded-lg focus:outline-none focus:border-brightOrange pl-4 py-2 placeholder:text-[14px]'
                 placeholder='1137 Williams Avenue'
@@ -145,75 +145,80 @@ const Checkout = () => {
               />
               {errors.address && <p>{errors.address.message}</p>}
             </div>
-            <div className='flex flex-col gap-2'>
-              <label className='text-[12px] font-bold'>Zip Code</label>
-              <input
-                className='border border-silver rounded-lg focus:outline-none focus:border-brightOrange pl-4 py-2 placeholder:text-[14px]'
-                placeholder='10001'
-                {...register('zip')}
-              />
-              {errors.zip && <p>{errors.zip.message}</p>}
-            </div>
-            <div className='flex flex-col gap-2'>
-              <label className='text-[12px] font-bold'>City</label>
-              <input
-                className='border border-silver rounded-lg focus:outline-none focus:border-brightOrange pl-4 py-2 placeholder:text-[14px]'
-                placeholder='New York'
-                {...register('city')}
-              />
-              {errors.city && <p>{errors.city.message}</p>}
-            </div>
-            <div className='flex flex-col gap-2'>
-              <label className='text-[12px] font-bold'>Country</label>
-              <input
-                className='border border-silver rounded-lg focus:outline-none focus:border-brightOrange pl-4 py-2 placeholder:text-[14px]'
-                placeholder='United States'
-                {...register('country')}
-              />
-              {errors.country && <p>{errors.country.message}</p>}
+            <div className='flex flex-col gap-6 md:grid md:grid-cols-2 md:gap-6'>
+              <div className='flex flex-col gap-2'>
+                <label className='text-[12px] font-bold'>Zip Code</label>
+                <input
+                  className='border border-silver rounded-lg focus:outline-none focus:border-brightOrange pl-4 py-2 placeholder:text-[14px]'
+                  placeholder='10001'
+                  {...register('zip')}
+                />
+                {errors.zip && <p>{errors.zip.message}</p>}
+              </div>
+              <div className='flex flex-col gap-2'>
+                <label className='text-[12px] font-bold'>City</label>
+                <input
+                  className='border border-silver rounded-lg focus:outline-none focus:border-brightOrange pl-4 py-2 placeholder:text-[14px]'
+                  placeholder='New York'
+                  {...register('city')}
+                />
+                {errors.city && <p>{errors.city.message}</p>}
+              </div>
+              <div className='flex flex-col gap-2'>
+                <label className='text-[12px] font-bold'>Country</label>
+                <input
+                  className='border border-silver rounded-lg focus:outline-none focus:border-brightOrange pl-4 py-2 placeholder:text-[14px]'
+                  placeholder='United States'
+                  {...register('country')}
+                />
+                {errors.country && <p>{errors.country.message}</p>}
+              </div>
             </div>
           </div>
           <h3 className='text-brightOrange text-subtitle uppercase mb-4'>
             Payment Details
           </h3>
-          <h2 className='font-bold text-[12px]'>Payment Method</h2>
-          <div className='flex flex-col'>
+          <div className='md:grid md:grid-cols-2'>
+            <h2 className='font-bold text-[12px]'>Payment Method</h2>
             <div>
-              <input
-                type='radio'
-                id='eMoney'
-                value='eMoney'
-                {...register('paymentMethod')}
-                checked={paymentMethod === 'eMoney'}
-                onChange={handlePaymentMethodChange}
-              />
-              <label
-                htmlFor='eMoney'
-                className='cursor-pointer text-[14px] border border-silver flex items-center truncate font-semibold rounded-lg py-2 pl-[52px]'>
-                e-Money
-              </label>
-            </div>
-
-            <div>
-              <input
-                type='radio'
-                id='cashOnDelivery'
-                value='cashOnDelivery'
-                {...register('paymentMethod')}
-                checked={paymentMethod === 'cashOnDelivery'}
-                onChange={handlePaymentMethodChange}
-              />
-              <label
-                htmlFor='cashOnDelivery'
-                className='cursor-pointer text-[14px] border border-silver flex items-center truncate font-semibold rounded-lg py-2 pl-[52px]'>
-                Cash On Delivery
-              </label>
+              <div className='flex flex-col'>
+                <div className='md:-mt-4'>
+                  <input
+                    type='radio'
+                    id='eMoney'
+                    value='eMoney'
+                    {...register('paymentMethod')}
+                    checked={paymentMethod === 'eMoney'}
+                    onChange={handlePaymentMethodChange}
+                  />
+                  <label
+                    htmlFor='eMoney'
+                    className='cursor-pointer text-[14px] border border-silver flex items-center truncate font-semibold rounded-lg py-2 pl-[52px]'>
+                    e-Money
+                  </label>
+                </div>
+              </div>
+              <div>
+                <input
+                  type='radio'
+                  id='cashOnDelivery'
+                  value='cashOnDelivery'
+                  {...register('paymentMethod')}
+                  checked={paymentMethod === 'cashOnDelivery'}
+                  onChange={handlePaymentMethodChange}
+                />
+                <label
+                  htmlFor='cashOnDelivery'
+                  className='cursor-pointer text-[14px] border border-silver flex items-center truncate font-semibold rounded-lg py-2 pl-[52px]'>
+                  Cash On Delivery
+                </label>
+              </div>
             </div>
           </div>
           {errors.paymentMethod && <p>{errors.paymentMethod.message}</p>}
           {paymentMethod === 'eMoney' && (
             <motion.div
-              className='mt-8 flex flex-col gap-6'
+              className='mt-8 flex flex-col gap-6 md:grid md:grid-cols-2'
               initial={{ opacity: 0, y: -20 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: -20 }}
@@ -238,8 +243,20 @@ const Checkout = () => {
               </div>
             </motion.div>
           )}
+          <div className='hidden lg:flex lg:items-center lg:justify-between lg:gap-8 lg:mt-[30px]'>
+            <img
+              className='w-[48px] h-[48px]'
+              src='/assets/checkout/icon-cash-on-delivery.svg'
+              alt=''
+            />
+            <p className='text-body opacity-50'>
+              The ‘Cash on Delivery’ option enables you to pay in cash when our
+              delivery courier arrives at your residence. Just make sure your
+              address is correct so that your order will not be cancelled.
+            </p>
+          </div>
         </div>
-        <div className='container mx-auto max-w-[327px] bg-white px-6 pt-6 pb-8 rounded-lg'>
+        <div className='container mx-auto max-w-[327px] md:max-w-[689px] lg:max-w-[350px] lg:h-1/2 bg-white px-6 pt-6 pb-8 rounded-lg'>
           <h3 className='text-h6 uppercase mb-8'>Summary</h3>
           <div className='flex flex-col gap-6'>
             <div>
